4019fb8e01772d58758218ccffc96c7a4a6fbc39
Gerd Reiss <gerdreiss@jscriptive.com>
2018-08-11 11:44:52 +0200

    boolean monoids

---
+++ src/main/scala/catz/monoids/BooleanMonoids.scala
@@ -0,0 +1,24 @@
+package catz.monoids
+
+import cats.Monoid
+
+object BooleanMonoids {
+
+  implicit val booleanAndM: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+  implicit val booleanOrM: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+  implicit val booleanXOrM: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = (x && !y) || (!x && y)
+  }
+  implicit val booleanXNorM: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+    override def combine(x: Boolean, y: Boolean): Boolean = (x || !y) && (!x || y)
+  }
+
+}
