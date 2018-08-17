090fa3ac26c85a61f2f52b9fe8db7a13f96f1c46
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-08-17 15:44:29 +0200

    Functors

---
+++ src/main/scala/com/github/learningcats/functors/Trees.scala
@@ -0,0 +1,26 @@
+package com.github.learningcats.functors
+
+import cats.Functor
+import cats.syntax.functor._
+
+object Trees extends App {
+
+  sealed trait Tree[A]
+  final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+  final case class Leaf[A](value: A) extends Tree[A]
+
+  object Tree {
+    def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+    def leaf[A](value: A): Tree[A] = Leaf(value)
+  }
+
+  implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Branch(left, right) => Branch(left map f, right map f)
+      case Leaf(value) => Leaf(f(value))
+    }
+  }
+
+  println(Tree.leaf(100).map(_ * 2))
+  println(Tree.branch(Tree.leaf(10), Tree.leaf(20)).map(_ * 2))
+}
