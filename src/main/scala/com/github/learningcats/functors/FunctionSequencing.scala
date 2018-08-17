140b3ec25e490750fb38653b2e27c250f98cd97b
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-08-17 15:33:02 +0200

    Functors

---
+++ src/main/scala/com/github/learningcats/functors/FunctionSequencing.scala
@@ -0,0 +1,16 @@
+package com.github.learningcats.functors
+
+import cats.instances.function._
+import cats.syntax.functor._
+
+object FunctionSequencing extends App {
+
+  val f1 = (x: Int) => x.toDouble
+  val f2 = (d: Double) => d + 1
+  val f3 = (d: Double) => d * 2
+  val f4 = (d: Double) => d + "!"
+
+  val funcSeq = f1 map f2 map f3 map f4
+
+  println(funcSeq(10))
+}
