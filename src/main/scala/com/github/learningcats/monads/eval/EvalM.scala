021f2fe8cfe9bc11aff47c5dd01ac940f5a0ed1d
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-08-29 16:58:02 +0200

    Monads: Eval

---
+++ src/main/scala/com/github/learningcats/monads/eval/EvalM.scala
@@ -0,0 +1,14 @@
+package com.github.learningcats.monads.eval
+
+import cats.Eval
+
+object EvalM extends App {
+
+  def factorial(n: BigInt): Eval[BigInt] =
+    if (n == 1) Eval.now(1)
+    // w/o Eval.defer this would end in StackOverflowError when using sufficiently high n
+    // w Eval.defer this still could end in OutOfMemoryError when using sufficiently high n
+    else Eval.defer(factorial(n - 1) map (_ * n))
+
+  println(factorial(50000).value)
+}
