fa8e00e8a83e989ac61779a9fa2d8d97c1281b0b
Gerd Reiss <gerd@reiss.pro>
2022-01-16 08:54:18 +0100

    Rock the JVM: Cats 2 - Monad, Part 2 + Exercise

---
+++ courses/rockthejvm/cats-2/src/main/scala/exercises/RudimentaryMonad.scala
@@ -0,0 +1,7 @@
+package exercises
+
+trait RudimentaryMonad[M[_]]:
+  def pure[A](value: A): M[A]
+  def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+  def map[A, B](ma: M[A])(f: A => B): M[B] =
+    flatMap(ma)(a => pure(f(a)))
--- courses/rockthejvm/cats-2/src/main/scala/lectures/algebra/Monads.scala
+++ courses/rockthejvm/cats-2/src/main/scala/lectures/algebra/Monads.scala
@@ -1,16 +1,25 @@
 package lectures.algebra
 
 import cats.Monad
+// for pure()
+import cats.syntax.applicative.*
+// for flatMap()
 import cats.syntax.flatMap.*
+// for map()
 import cats.syntax.functor.*
+// for sequence()
 import cats.syntax.traverse.*
 
 // generic API
-def mkTupleM[M[_]: Monad, A, B](fa: M[A], fb: M[B]): M[(A, B)] =
+def mkTupleContextM[M[_]: Monad, A, B](ma: M[A], mb: M[B]): M[(A, B)] =
+  // ma.flatMap(a => mb.map(b => (a, b)))
   for
-    a <- fa
-    b <- fb
+    a <- ma
+    b <- mb
   yield (a, b)
+
+def mkTupleUsingM[M[_], A, B](ma: M[A], mb: M[B])(using M: Monad[M]): M[(A, B)] =
+  M.flatMap(ma)(a => M.map(mb)(b => (a, b)))
 
 @main def monads(): Unit =
   val numbers = List(1, 2, 3)
@@ -41,5 +50,15 @@ def mkTupleM[M[_]: Monad, A, B](fa: M[A], fb: M[B]): M[(A, B)] =
   val numbersMonad = listMonad.pure(42) // List(42)
   val numbersMonadAppendInc = listMonad.flatMap(numbersMonad)(x => List(x, x + 1))
 
-  println(mkTupleM(numbers, chars))
-  println(mkTupleM(maybeNumber, maybeChar))
+  println(mkTupleContextM(numbers, chars))
+  println(mkTupleContextM(maybeNumber, maybeChar))
+  println(mkTupleUsingM(numbers, chars))
+  println(mkTupleUsingM(maybeNumber, maybeChar))
+
+  // extension methods
+  val maybeOne = 1.pure[Option]
+  val listOfOne = 1.pure[List]
+  val maybeOneIncr = maybeOne
+    .withFilter(_ > 0)
+    .map(_ + 1)
+    .flatMap(incrd => (incrd + 1).pure[Option])
