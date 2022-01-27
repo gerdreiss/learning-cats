eeea2b389aa559fd64c602b6b3053330021d139a
Gerd Reiss <gerd@reiss.pro>
2022-01-27 18:50:51 +0100

    minor code improvements

--- courses/rockthejvm/cats-effect-3/src/main/scala/coordination/Semaphores.scala
+++ courses/rockthejvm/cats-effect-3/src/main/scala/coordination/Semaphores.scala
@@ -2,17 +2,18 @@ package coordination
 
 import cats.effect.*
 import cats.effect.std.Semaphore
-import scala.util.Random
-import scala.concurrent.duration.*
-import utils.*
 import cats.syntax.parallel.*
+import utils.*
+
+import scala.concurrent.duration.*
+import scala.util.Random
 
 object Semaphores extends IOApp.Simple:
 
   // val semaphore: IO[Semaphore[IO]] = Semaphore[IO](2) // 2 total permits
 
   // example: limiting the number of concurrent sessions on a server
-  def computeNum: IO[Int] = IO.sleep(1.second) >> IO(Random.nextInt(100))
+  def computeNum: IO[Int] = IO.sleep(1.second) *> IO(Random.nextInt(100))
 
   def login(id: Int, sem: Semaphore[IO]): IO[Int] =
     for
