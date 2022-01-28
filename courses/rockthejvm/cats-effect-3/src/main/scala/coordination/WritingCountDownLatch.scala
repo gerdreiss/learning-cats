1cda0bfc3e1f8c9a0c2988ce8aadc707e39b0360
Gerd Reiss <gerd@reiss.pro>
2022-01-28 07:16:40 +0100

    Rock the JVM: Cats Effect 3 - CountDownLatch

--- courses/rockthejvm/cats-effect-3/src/main/scala/coordination/WritingCountDownLatch.scala
+++ courses/rockthejvm/cats-effect-3/src/main/scala/coordination/WritingCountDownLatch.scala
@@ -20,14 +20,20 @@ object WritingCountDownLatch extends IOApp.Simple:
         signal <- Deferred[IO, Unit]
         state  <- Ref[IO].of[State](State.Live(count, signal))
       yield new CBL:
-        override def await: IO[Unit]   = state.get.flatMap { s =>
-          if s == State.Done then IO.unit // continue, the latch is dead
-          else signal.get
-        }
-        override def release: IO[Unit] = state.modify {
-          case State.Live(1, signal) => State.Done                -> signal.complete(()).void
-          case State.Live(n, signal) => State.Live(n - 1, signal) -> IO.unit
-          case State.Done            => State.Done                -> IO.unit
-        }.flatten
+        override def await: IO[Unit]   =
+          state.get
+            .flatMap { s =>
+              if s == State.Done then IO.unit // continue, the latch is dead
+              else signal.get
+            }
+        override def release: IO[Unit] =
+          state
+            .modify {
+              case State.Live(1, signal) => State.Done                -> signal.complete(()).void
+              case State.Live(n, signal) => State.Live(n - 1, signal) -> IO.unit
+              case State.Done            => State.Done                -> IO.unit
+            }
+            .flatten
+            .uncancelable
 
   override def run = ???
