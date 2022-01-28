81e8f320e395a42309f0e2cf72ae3c25d213ab25
Gerd Reiss <gerd@reiss.pro>
2022-01-28 11:18:18 +0100

    Rock the JVM: Cats Effect 3 - Cyclic Barriers

---
+++ courses/rockthejvm/cats-effect-3/src/main/scala/coordination/CyclicBarriers.scala
@@ -0,0 +1,44 @@
+package coordination
+
+import cats.effect.*
+import cats.effect.std.*
+import cats.syntax.parallel.*
+
+import utils.*
+
+import scala.concurrent.duration.*
+
+object CyclicBarriers extends IOApp.Simple:
+
+  /**
+   * A cyclic barrier is a coordination primitive that
+   * - is initialized with a count
+   * - has a single API: await
+   * 
+   * A cyclic barrier will (semantically) block all fibers calling its await() emthod
+   * until we have exactly N fibers waiting, at which p;oint the barrier will unblock all fibers
+   * and reset to its original state.
+   * Any further fiber will again block until we have exactly N fibers waiting.
+   * 
+   * ...
+   * 
+   * And so on.
+  */
+  def createUser(id: Int, barrier: CyclicBarrier[IO]): IO[Unit] =
+    for
+      _ <- IO.sleep((scala.util.Random.nextDouble * 500).toInt.millis)
+      _ <- IO(s"[user $id] just heard there's this new thing - signing up... ").debug
+      _ <- IO.sleep((scala.util.Random.nextDouble * 1500).toInt.millis)
+      _ <- IO(s"[user $id] On the waiting list now... ").debug
+      _ <- barrier.await // block the fiber when there are exactly N users waiting
+      _ <- IO(s"[user $id] I'm in... ").debug
+    yield ()
+
+  def openNetwork: IO[Unit] =
+    for
+      _       <- IO("[announcer] network is up for reg")
+      barrier <- CyclicBarrier[IO](10)
+      _       <- (1 to 9).toList.parTraverse(id => createUser(id, barrier))
+    yield ()
+
+  override def run = openNetwork
