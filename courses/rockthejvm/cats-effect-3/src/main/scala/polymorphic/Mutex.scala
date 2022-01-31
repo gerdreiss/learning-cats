45cb92b857728d72684ea67dfc365f8fd786872b
Gerd Reiss <gerd@reiss.pro>
2022-01-31 05:21:15 +0100

    Rock the JVM: Cats Effect 3 - Polymorphic Coordination, Exercise

---
+++ courses/rockthejvm/cats-effect-3/src/main/scala/polymorphic/Mutex.scala
@@ -0,0 +1,102 @@
+package polymorphic
+
+import utils.generic.*
+import cats.*
+import cats.effect.*
+import cats.syntax.applicative.*
+import cats.syntax.functor.*
+import cats.syntax.flatMap.*
+import cats.syntax.parallel.*
+import cats.effect.syntax.spawn.*
+import cats.effect.syntax.monadCancel.*
+import scala.collection.immutable.Queue
+import scala.concurrent.duration.*
+import scala.util.Random
+import cats.effect.kernel.Outcome.Succeeded
+import cats.effect.kernel.Outcome.Errored
+import cats.effect.kernel.Outcome.Canceled
+
+abstract class Mutex[F[_]]:
+  def acquire: F[Unit]
+  def release: F[Unit]
+
+object Mutex:
+
+  type Signal[F[_]] = Deferred[F, Unit]
+
+  case class State[F[_]](locked: Boolean, waiting: Queue[Signal[F]])
+
+  def _unlocked[F[_]] = State[F](locked = false, Queue.empty)
+
+  def _create[F[_]](state: Ref[F, State[F]])(using C: Concurrent[F]): Mutex[F] =
+    new:
+      override def acquire: F[Unit] =
+        C.uncancelable { poll =>
+          C.deferred[Unit].flatMap { signal =>
+
+            val cleanup = state.modify { s =>
+              s.copy(waiting = s.waiting.filterNot(_ eq signal)) -> release
+            }.flatten
+
+            state.modify {
+              case State(false, _)    =>
+                State(locked = true, Queue.empty) -> C.unit
+              case State(true, queue) =>
+                State(locked = true, queue.enqueue(signal)) -> poll(signal.get).onCancel(cleanup)
+            }.flatten
+          }
+        }
+
+      override def release: F[Unit] =
+        state.modify { state =>
+          if state.locked && state.waiting.nonEmpty then
+            val (signal, rest) = state.waiting.dequeue
+            State(locked = true, rest) -> signal.complete(()).void
+          else _unlocked -> C.unit
+        }.flatten
+
+  def create[F[_]](using C: Concurrent[F]): F[Mutex[F]] =
+    C.ref(_unlocked).map(_create)
+
+end Mutex
+
+object MutexDemo extends IOApp.Simple:
+
+  def criticalTask[F[_]: Concurrent]: F[Int] =
+    unsafeSleep(1.second) >> Random.nextInt(100).pure
+
+  def createLockingTask[F[_]: Concurrent](id: Int, mutex: Mutex[F]): F[Int] =
+    for
+      _   <- s"[task $id] waiting for permission...".pure.debug
+      _   <- mutex.acquire // this will block if the mutex has been acquired by some other fiber
+      // critical section
+      _   <- s"[task $id] working...".pure.debug
+      res <- criticalTask
+      _   <- s"[task $id] got result: $res".pure.debug
+      // critical section end
+      _   <- mutex.release
+      _   <- s"[task $id] lock removed.".pure.debug
+    yield res
+
+  def createCancellingTask[F[_]: Concurrent](id: Int, mutex: Mutex[F]): F[Int] =
+    if id % 2 == 0 then createLockingTask(id, mutex)
+    else
+      for
+        fib    <- createLockingTask(id, mutex)
+                    .onCancel(s"[task $id] received cancellation!".pure.debug.void)
+                    .start
+        _      <- unsafeSleep(2.seconds) >> fib.cancel
+        out    <- fib.join
+        result <- out match
+                    case Succeeded(effect) => effect
+                    case Errored(_)        => -1.pure
+                    case Canceled()        => -2.pure
+      yield result
+
+  def demoCancellingTask[F[_]: Concurrent: Parallel]: F[List[Int]] =
+    for
+      mutex   <- Mutex.create
+      results <- (1 to 10).toList.parTraverse(id => createCancellingTask(id, mutex))
+    yield results
+
+  override def run = demoCancellingTask[IO].debug.void
--- courses/rockthejvm/cats-effect-3/src/main/scala/polymorphic/PolymorphicCoordination.scala
+++ courses/rockthejvm/cats-effect-3/src/main/scala/polymorphic/PolymorphicCoordination.scala
@@ -1,9 +1,12 @@
 package polymorphic
 
 import cats.effect.*
-import cats.syntax.all.*
-import cats.effect.syntax.all.*
+import cats.effect.syntax.monadCancel.*
+import cats.effect.syntax.spawn.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
 import utils.generic.*
+
 import scala.concurrent.duration.*
 
 object PolymorphicCoordination extends IOApp.Simple:
@@ -44,5 +47,33 @@ object PolymorphicCoordination extends IOApp.Simple:
       _               <- notificationFib.join
       _               <- clock.join
     yield ()
+
+  // generalize racePair
+  type RaceResult[F[_], A, B] = Either[
+    (Outcome[F, Throwable, A], Fiber[F, Throwable, B]), // (winner result, loser fiber)
+    (Fiber[F, Throwable, A], Outcome[F, Throwable, B])  // (loser fiber, winner result)
+  ]
+
+  type EitherOutcome[F[_], A, B] = Either[Outcome[F, Throwable, A], Outcome[F, Throwable, B]]
+
+  def racePair[F[_], A, B](fa: F[A], fb: F[B])(using C: Concurrent[F]): F[RaceResult[F, A, B]] =
+    C.uncancelable { poll =>
+      for
+        signal <- C.deferred[EitherOutcome[F, A, B]]
+        fibA   <- fa.guaranteeCase(outcomeA => signal.complete(Left(outcomeA)).void).start
+        fibB   <- fb.guaranteeCase(outcomeB => signal.complete(Right(outcomeB)).void).start
+        result <- poll(signal.get) // Blocking call - should be cancellable
+                    .onCancel {
+                      for
+                        cancelFibA <- fibA.cancel.start
+                        cancelFibB <- fibB.cancel.start
+                        _          <- cancelFibA.join
+                        _          <- cancelFibB.join
+                      yield ()
+                    }
+      yield result match
+        case Left(outcomeA)  => Left((outcomeA, fibB))
+        case Right(outcomeB) => Right((fibA, outcomeB))
+    }
 
   override def run = polymorphicEggBoiler
