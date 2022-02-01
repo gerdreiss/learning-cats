8b9f1c16f95e8fc96043e34cfc3b741d041b6d84
Gerd Reiss <gerd@reiss.pro>
2022-02-01 08:32:23 +0100

    Rock the JVM: Cats Effect 3 - Polymorphic Async, Exercises

--- courses/rockthejvm/cats-effect-3/src/main/scala/polymorphic/PolymorphicAsync.scala
+++ courses/rockthejvm/cats-effect-3/src/main/scala/polymorphic/PolymorphicAsync.scala
@@ -1,6 +1,10 @@
 package polymorphic
 
+import cats.*
 import cats.effect.*
+import cats.syntax.applicative.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
 import utils.generic.*
 
 import java.util.concurrent.Executors
@@ -10,10 +14,13 @@ object PolymorphicAsync extends IOApp.Simple:
 
   trait CustomAsync[F[_]] extends Sync[F] with Temporal[F]:
     def executionContext: F[ExecutionContext]
-    def async[A](callback: (Either[Throwable, A] => Unit) => IO[Option[IO[Unit]]]): F[A]
-    def async_[A](callback: (Either[Throwable, A] => Unit) => Unit): F[A]
     def evalOn[A](fa: F[A], ec: ExecutionContext): F[A]
-    def never[A]: F[A] // never-ending computation
+    def async[A](callback: (Either[Throwable, A] => Unit) => F[Option[F[Unit]]]): F[A]
+
+    def async_[A](callback: (Either[Throwable, A] => Unit) => Unit): F[A] =
+      async(cb => map(pure(callback(cb)))(_ => None))
+
+    def never[A]: F[A] = async_(_ => ())
 
   val asyncIO = Async[IO]
 
@@ -40,5 +47,18 @@ object PolymorphicAsync extends IOApp.Simple:
       }
     }.as(Some(IO("cancelled!").debug.void))
   }
+
+  // exercises
+  // 1 - implement async_ and never with async
+  // tuple two effects with diff. requirements
+
+  def effect1[F[_]: Concurrent, A](a: A): F[A] = a.pure[F]
+  def effect2[F[F]: Sync, A](a: A): F[A]       = a.pure[F]
+
+  def effectTuple[F[_]: Async, A](a: A): F[(A, A)] =
+    for
+      eff1 <- effect1(a)
+      eff2 <- effect2(a)
+    yield (eff1, eff2)
 
   override def run = ???
