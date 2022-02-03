939688cf2e58f42b71bd278932738c13b6789b9e
Gerd Reiss <gerd@reiss.pro>
2022-02-03 09:59:06 +0100

    trampolining recursive applications

--- courses/rockthejvm/cats-effect-3/build.sbt
+++ courses/rockthejvm/cats-effect-3/build.sbt
@@ -6,5 +6,6 @@ lazy val root = project
     name         := "cats-effect-3",
     version      := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
+    scalacOptions ++= Seq("-language:postfixOps"),
     libraryDependencies ++= Seq("org.typelevel" %% "cats-effect" % "3.3.5")
   )
--- courses/rockthejvm/cats-effect-3/src/main/scala/Playground.scala
+++ courses/rockthejvm/cats-effect-3/src/main/scala/Playground.scala
@@ -1,32 +1,27 @@
 import cats.effect.*
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
+import scala.util.Failure
 
 object Playground extends IOApp.Simple:
 
-  val nums = (1 to 1000000).toList
+  val nums = (1 to 10000000).toList
 
-  def len[A](as: List[A]): Int =
+  def len[A](as: List[A]): BigInt =
     if as.isEmpty then 0 else 1 + len(as.tail)
+
+  def lenIO[A](as: List[A]): IO[BigInt] =
+    if as.isEmpty then IO.pure(0)
+    else IO.defer(lenIO(as.tail).map(_ + 1))
 
   def lenFuture[A](as: List[A]): Future[Int] =
     if as.isEmpty then Future(0)
-    else
-      for
-        one  <- Future(1)
-        rest <- Future.delegate(lenFuture(as.tail))
-      yield one + rest
-
-  def lenIO[A](as: List[A]): IO[Int] =
-    if as.isEmpty then IO.pure(0)
-    else
-      for
-        one  <- IO.pure(1)
-        rest <- IO.defer(lenIO(as.tail))
-      yield one + rest
+    else Future.delegate(lenFuture(as.tail).map(_ + 1))
 
   override def run: IO[Unit] =
-    IO.println("Learning Cats Effect 3!")
+    IO.println("Learning Cats Effect 3!") *>
 
-    lenIO(nums).map(println)
+    // len(nums)
+    // lenIO(nums).map(println)
     IO.fromFuture(IO(lenFuture(nums))).map(println)
