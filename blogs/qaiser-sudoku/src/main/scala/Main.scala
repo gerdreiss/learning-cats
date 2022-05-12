ce3ad489966733e6c09626d51da3a478cf3ed47b
Gerd Reiss <gerd@reiss.pro>
2022-05-12 20:24:25 +0200

    [Concurrent Sudoku Solver] Part 2 - Using Cats Effect Ref + Deferred + IO.race

--- blogs/qaiser-sudoku/src/main/scala/Main.scala
+++ blogs/qaiser-sudoku/src/main/scala/Main.scala
@@ -1,3 +1,6 @@
+import cats.effect.*
+import cats.implicits.*
+
 trait Solver[F[_]]:
   def solve(givens: List[Value.Given]): F[List[Value]]
 
@@ -46,6 +49,62 @@ object Candidate:
         case multipleCandidates     => Multiple(coord, multipleCandidates.toSet)
 
   def initial(coord: Coord): Multiple = Multiple(coord, (1 to 9).toSet)
+
+trait Cell:
+  def coord: Coord
+  def deduceSingleCandidate(allCells: List[Cell]): IO[Value]
+  protected[this] def deferredValue: Deferred[IO, Value]
+
+  def getValue: IO[Value] = deferredValue.get
+
+  def solve(givensMap: Map[Coord, Value.Given], allCells: List[Cell]): IO[Value] =
+    (givensMap.get(coord) match
+      case Some(givenValue) => IO.pure(givenValue)
+      case None             => deduceSingleCandidate(allCells)
+    ).flatTap(deferredValue.complete)
+
+object Cell:
+  def make(_coord: Coord): IO[Cell] =
+    for _deferredValue <- Deferred[IO, Value]
+    yield new Cell:
+      override val coord: Coord = _coord
+
+      override val deferredValue: Deferred[IO, Value] = _deferredValue
+
+      override def deduceSingleCandidate(allCells: List[Cell]): IO[Candidate.Single] =
+        for
+          refCandidate                <- Ref.of[IO, Candidate](Candidate.initial(coord))
+          peerCells                    = allCells.filter(_.coord.isPeerOf(coord))
+          listOfSingleCandidateOrNever =
+            peerCells.map(peerCell => refineToSingleCandidateOrNever(refCandidate, peerCell))
+          singleCandidate             <- raceMany(listOfSingleCandidateOrNever)
+        yield singleCandidate
+
+      private def raceMany[T](listOfIOs: List[IO[T]]) =
+        listOfIOs.reduce((a, b) => a.race(b).map(_.merge))
+
+      private def refineToSingleCandidateOrNever(
+          refCandidate: Ref[IO, Candidate],
+          peerCell: Cell
+      ): IO[Candidate.Single] =
+        for
+          peerValue       <- peerCell.getValue
+          singleCandidate <- refCandidate.modify {
+                               case multiple: Candidate.Multiple    =>
+                                 multiple.refine(peerValue) match
+                                   case single: Candidate.Single     => (single, IO.pure(single))
+                                   case multiple: Candidate.Multiple => (multiple, IO.never)
+                               case alreadySingle: Candidate.Single => (alreadySingle, IO.never)
+                             }.flatten
+        yield singleCandidate
+
+object CatsEffectDeferredRefRaceSolver extends Solver[IO]:
+  def solve(givens: List[Value.Given]): IO[List[Value]] =
+    for
+      allCells <- Coord.allCoords.traverse(Cell.make)
+      givensMap = givens.map(g => g.coord -> g).toMap
+      values   <- allCells.parTraverse(_.solve(givensMap, allCells))
+    yield values
 
 @main def hello: Unit =
   println("Hello world!")
