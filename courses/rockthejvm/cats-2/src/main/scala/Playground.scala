c64996c82cdb4478ff6cbdd7929ad5a8c5b894c7
Gerd Reiss <gerd@reiss.pro>
2022-05-24 11:13:57 +0200

    [Cats] testing the guard

--- courses/rockthejvm/cats-2/src/main/scala/Playground.scala
+++ courses/rockthejvm/cats-2/src/main/scala/Playground.scala
@@ -1,1 +1,5 @@
-@main def playground: Unit = ???
+import cats.implicits.*
+import scala.util.Try
+
+@main def playground: Unit =
+  println(false.guard[Option].as(42).liftTo[Try](new Exception("Boom!")))
